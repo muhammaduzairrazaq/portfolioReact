239182a6b5a15f6f519e4838c8c6341a637863c9
muhammaduzairrazaq <muhammaduzairrazaq@gmail.com>
2023-10-22 00:31:50 +0500

    added banner.js

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/NavBar'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import { Banner } from './components/Banner';
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
+      <Banner></Banner>
     </div>
   );
 }
